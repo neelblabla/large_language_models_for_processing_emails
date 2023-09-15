5160907ccbc8e49b2389743a67405f33b29ba611
nilsmart96 <n.marthiensen@gmail.com>
2023-09-15 12:19:25 +0200

    Minor changes

--- 1_data_preparation_and_eda/create_labelled_data.py
+++ 1_data_preparation_and_eda/create_labelled_data.py
@@ -11,5 +11,5 @@ df = pd.read_csv("../brianray-enron-email-dataset/data/enron_05_17_2015_with_lab
 # Filter rows where column "labeled" is "True"
 df_filtered = df[df["labeled"] == True]
 
-# Now, "df_filtered" contains the filtered rows with "True" in column 2
+# Now, "df_filtered" contains the filtered rows with "True" in column "labeled"
 df_filtered.to_csv("../labelled_enron.csv", index=False)
